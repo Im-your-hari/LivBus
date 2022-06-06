d4998a55ebd97e9dcb9114786e11b3eb54f6ac40
Im-your-hari <baluoger91@gmail.com>
2022-06-06 14:36:31 +0530

    Exit button works succesfull

--- app/src/main/java/com/example/livbus/MainActivity.kt
+++ app/src/main/java/com/example/livbus/MainActivity.kt
@@ -5,6 +5,7 @@ import android.widget.MediaController
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.VideoView
 import androidx.annotation.RequiresApi
 
@@ -14,14 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var exitBtn : Button = findViewById(R.id.exitBtn)
         var videoView : VideoView = findViewById(R.id.videoView)
         val mediaController : MediaController
+
         mediaController = MediaController(this)
         mediaController.setAnchorView(videoView)
         videoView.setVideoPath("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4")
         videoView.start()
         videoView.setMediaController(mediaController)
 
+        exitBtn.setOnClickListener(){
+            finish()
+        }
 
     }
 
