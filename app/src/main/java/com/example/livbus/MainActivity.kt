abbd5a6cf23dac08470b89a59fdb453a16c39289
Im-your-hari <baluoger91@gmail.com>
2022-06-06 13:14:39 +0530

    RTSP Video playing is done

--- app/src/main/java/com/example/livbus/MainActivity.kt
+++ app/src/main/java/com/example/livbus/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.livbus
 
 
+import android.widget.MediaController
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.VideoView
+import androidx.annotation.RequiresApi
 
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var videoView : VideoView = findViewById(R.id.videoView)
+        val mediaController : MediaController
+        mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        videoView.setVideoPath("rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4")
+        videoView.start()
+        videoView.setMediaController(mediaController)
     }
+
+
 }
 
