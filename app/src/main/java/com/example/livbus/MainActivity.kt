b36e9d071885234d5d90763f6855d09db0b1f983
Im-your-hari <baluoger91@gmail.com>
2022-06-07 12:01:35 +0530

    Recording will be added shortly

--- app/src/main/java/com/example/livbus/MainActivity.kt
+++ app/src/main/java/com/example/livbus/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.livbus
 
 
+import android.app.DownloadManager
+import android.content.ContentValues
+import android.net.Uri
 import android.widget.MediaController
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.annotation.RequiresApi
 
@@ -17,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var exitBtn : Button = findViewById(R.id.exitBtn)
+        var recButton : Button = findViewById(R.id.recButton)
         var videoView : VideoView = findViewById(R.id.videoView)
         val mediaController : MediaController
 
@@ -26,11 +32,38 @@ class MainActivity : AppCompatActivity() {
         videoView.start()
         videoView.setMediaController(mediaController)
 
+        //println("Duration : "+mediaController.scrollBarFadeDuration)
+        //Log.i("The Duration is :: ", mediaController.scrollBarFadeDuration.toString())
+        /*URI PARSING METHOD
+        videoView.setVideoURI(
+            Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
+        )
+
+        */
+
         exitBtn.setOnClickListener(){
             finish()
         }
 
+        recButton.setOnClickListener(){
+            /*val request = DownloadManager.Request(Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"))
+                .setTitle("Cartoon")
+                .setDescription("Downloading..!")
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setAllowedOverMetered(true)
+
+            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
+
+            dm.enqueue(request)
+            */
+            Toast.makeText(this,"Recording will be add shortly..",Toast.LENGTH_LONG).show()
+
+        }
+
     }
+
+
+
 
 
 }
