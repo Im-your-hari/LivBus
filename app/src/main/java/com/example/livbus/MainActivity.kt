ec964e9609e7d69fc0dfa4f772df28f7e254e64e
Im-your-hari <baluoger91@gmail.com>
2022-06-25 17:26:14 +0530

    Need to check database structure...And arrange it..

--- app/src/main/java/com/example/livbus/MainActivity.kt
+++ app/src/main/java/com/example/livbus/MainActivity.kt
@@ -2,33 +2,41 @@ package com.example.livbus
 
 
 import android.app.DownloadManager
-import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.net.Uri
-import android.widget.MediaController
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.Toast
-import android.widget.VideoView
+import android.util.Log
+import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
 import java.io.File
-import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database : FirebaseDatabase
+
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var exitBtn : Button = findViewById(R.id.exitBtn)
-        var recButton : Button = findViewById(R.id.recButton)
-        var videoView : VideoView = findViewById(R.id.videoView)
+        val exitBtn : Button = findViewById(R.id.exitBtn)
+        val recButton : Button = findViewById(R.id.recButton)
+        val videoView : VideoView = findViewById(R.id.videoView)
+        val stdId : TextView = findViewById(R.id.stdView)
         val mediaController : MediaController
+
+        val fDatabase : FirebaseDatabase
+        var dRef : DatabaseReference
+        fDatabase = FirebaseDatabase.getInstance()
+
+
+
 
         mediaController = MediaController(this)
         mediaController.setAnchorView(videoView)
@@ -36,44 +44,48 @@ class MainActivity : AppCompatActivity() {
         videoView.start()
         videoView.setMediaController(mediaController)
 
-        //println("Duration : "+mediaController.scrollBarFadeDuration)
-        //Log.i("The Duration is :: ", mediaController.scrollBarFadeDuration.toString())
-        /*URI PARSING METHOD
-        videoView.setVideoURI(
-            Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4")
-        )
 
-        */
 
-        exitBtn.setOnClickListener(){
+
+
+        exitBtn.setOnClickListener{
             finish()
         }
 
-        recButton.setOnClickListener(){
-            /*val request = DownloadManager.Request(Uri.parse("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"))
-                .setTitle("Cartoon")
-                .setDescription("Downloading..!")
-                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-                .setAllowedOverMetered(false)
-
-            val dm = getSystemService(DOWNLOAD_SERVICE) as DownloadManager
-
-            dm.enqueue(request)
-            */
-
+        recButton.setOnClickListener{
+            stdId.text = "PRP19CS029"
             download("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4","Bus")
+            //Data
+            dRef = fDatabase.getReference().child("student-id")
+            dRef.child("student_id").addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    val value = dataSnapshot.getValue(String::class.java)
+                    stdId.text = value
+                    stdId.text = "Need to check database structure...And arrange it.."
+                    Log.d(TAG, "Value is: $value")
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
 
 
-
+            //Data
         }
 
     }
 
+
+
     private fun download(url:String,fileName:String) {
         try {
-            var downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-            var videoLink = Uri.parse(url)
-            var request = DownloadManager.Request(videoLink)
+            val downloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+            val videoLink = Uri.parse(url)
+            val request = DownloadManager.Request(videoLink)
             request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
                 .setMimeType("video/mp4")
                 .setAllowedOverRoaming(false)
@@ -84,7 +96,7 @@ class MainActivity : AppCompatActivity() {
 
             Toast.makeText(this,"Downloading...",Toast.LENGTH_LONG).show()
         }catch (e:Exception){
-            Toast.makeText(this,"Recording failed...",Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"recording failed...",Toast.LENGTH_LONG).show()
         }
     }
 
