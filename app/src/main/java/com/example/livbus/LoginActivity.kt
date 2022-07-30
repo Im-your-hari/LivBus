75b2e525b3b837e1c2547986cc623c9f40615106
Im-your-hari <baluoger91@gmail.com>
2022-07-30 17:08:35 +0530

    Student id and name added to second activity

--- app/src/main/java/com/example/livbus/LoginActivity.kt
+++ app/src/main/java/com/example/livbus/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.example.livbus
 
 import android.app.DownloadManager
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -13,6 +14,7 @@ import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.*
+import com.google.firebase.database.core.Tag
 import java.io.File
 
 
@@ -26,10 +28,13 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         var loginBtn = findViewById<Button>(R.id.loginBtn)
-
+        val stdName = findViewById<EditText>(R.id.stdName).text.toString()
+        val stdId = findViewById<EditText>(R.id.stdId).text.toString()
         loginBtn.setOnClickListener{
+            Log.d(TAG, "Student Name => $stdName")
             var i = Intent(this,MainActivity::class.java)
             startActivity(i)
+
         }
 
     }
