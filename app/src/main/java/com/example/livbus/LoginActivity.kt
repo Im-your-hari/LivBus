43a8a207e0be7442f90e321b3285ff4d9a876e02
Im-your-hari <baluoger91@gmail.com>
2022-07-30 16:17:30 +0530

    Firebase video issue pending, Receive data from firebase, Check firebase data with login id of student

---
+++ app/src/main/java/com/example/livbus/LoginActivity.kt
@@ -0,0 +1,32 @@
+package com.example.livbus
+
+import android.app.DownloadManager
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.*
+import java.io.File
+
+
+
+class LoginActivity : AppCompatActivity() {
+
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+
+}
+
